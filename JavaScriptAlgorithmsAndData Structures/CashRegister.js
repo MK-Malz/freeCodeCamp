db34f414c5a409d8e1099cdcafb51ec4885cfa2e
MK-Malz <klane.malte@gmail.com>
2021-06-19 00:35:24 +0200

    Update CashRegister.js

--- JavaScriptAlgorithmsAndData Structures/CashRegister.js
+++ JavaScriptAlgorithmsAndData Structures/CashRegister.js
@@ -1,14 +1,21 @@
-function fundsSufficient(change, cid)
+function aggregateCash(cid)
 {
   var availableChange = 0;
   for(var i = 0; i < cid.length; i++)
   {
       availableChange += cid[i][1];
   }
-  return availableChange >= change ? true : false;
+  return availableChange;
 }
 
-function calculateChange(change, cid)
+
+function fundsSufficient(change, cid)
+{
+  
+  return aggregateCash(cid) >= change ? true : false;
+}
+
+function calculateChange(change, cid, statusMessage)
 {
   var returnChange = [
   ["PENNY", 0.0, 0.01],
@@ -33,34 +40,46 @@ function calculateChange(change, cid)
     }
     else
       i--
-    change = Math.round(change * 100) / 100    
+    change = Math.round(change * 100) / 100;    
   }
 
   var formatedChange = [];
-  for(var j = returnChange.length-1; j >= 0; j--)
+  if(statusMessage == "OPEN")
   {
-    if(returnChange[j][1] != 0)
+    for(var j = returnChange.length-1; j >= 0; j--)
+    {
+        if(returnChange[j][1] != 0)
+        {
+          returnChange[j].pop();
+          formatedChange.push(returnChange[j]);
+        }
+    }
+  }
+  else
+  {
+    for(var j = 0; j <= returnChange.length-1; j++)
     {
         returnChange[j].pop();
         formatedChange.push(returnChange[j]);
+        formatedChange[j][1] = Math.round(formatedChange[j][1] * 100) / 100 ;
     }
   }
+  
   return formatedChange;    
 }
 
 function checkCashRegister(price, cash, cid) 
 {
   var change = cash-price;
-  
+  var statusMessage = change == aggregateCash(cid) ? "CLOSED" : "OPEN";
+
   if(!fundsSufficient(change, cid))
     return {status: "INSUFFICIENT_FUNDS", change: []}
 
-  var calculatedChange = calculateChange(change, cid);
-
-  return {status: "OPEN", change: calculatedChange};
+  var calculatedChange = calculateChange(change, cid, statusMessage);
+  console.log({status: statusMessage, change: calculatedChange})
+  return {status: statusMessage, change: calculatedChange};
 }
 
-//checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
-
-checkCashRegister(100, 335.37, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 1400]])
-
+checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
+checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
