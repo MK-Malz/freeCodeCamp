64eb67963186d3b95d284097b46bdf1574f4e303
MK-Malz <klane.malte@gmail.com>
2021-06-18 21:09:12 +0200

    Update CaesarsCipher.js
    working solution

--- JavaScriptAlgorithmsAndData Structures/CaesarsCipher.js
+++ JavaScriptAlgorithmsAndData Structures/CaesarsCipher.js
@@ -1,6 +1,23 @@
 function rot13(str) {
-
-  return str;
+  var decodeString = ''
+  for(var i = 0; i < str.length; i++)
+  {
+      if(str.charCodeAt(i) >= 78 && str.charCodeAt(i) <= 90)
+      {
+        var j = str.charCodeAt(i)-13;
+        decodeString += String.fromCharCode(j);
+      }  
+      else if(str.charCodeAt(i) < 78 && str.charCodeAt(i) > 64)
+      {
+        var j = str.charCodeAt(i)+13;
+        decodeString += String.fromCharCode(j);
+      }
+      else
+      {
+          decodeString += str[i];
+      }       
+  }
+  return decodeString;
 }
 
 rot13("SERR PBQR PNZC");
