a457c6602d4f8bdb2488402467b5d2f60c4546d6
MK-Malz <klane.malte@gmail.com>
2022-01-03 15:34:37 +0100

    Update D3HeatMap.js

--- DataVisualization/D3HeatMap.js
+++ DataVisualization/D3HeatMap.js
@@ -15,38 +15,45 @@ var svg = d3
 //Read the data
 d3.json(
   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json",
-  function (data) {
-    let dataset = data.monthlyVariance;
-
-    let years = d3 //x Axis
+  function (data) {  
+      
+      let dataset = data.monthlyVariance
+    
+      let years = d3  //x Axis
       .map(dataset, function (d) {
         return d.year;
       })
       .keys();
-
-    let months = d3 //y-Acis
-      .map(dataset, function (d) {
+    
+    
+    let months = d3    //y-Acis
+     .map(dataset, function (d) {
         return d.month;
       })
       .keys();
-
+    
+    
     var xAxis = d3.scaleBand().range([0, width]).domain(years).padding(0.05);
-
+    
     svg
       .append("g")
       .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(xAxis).tickSize(0));
+      .call(d3.axisBottom(xAxis).tickSize(0))
+
 
     var yAxis = d3.scaleBand().range([height, 0]).domain(months).padding(0.05);
-
-    svg.append("g").call(d3.axisLeft(yAxis).tickSize(0));
-
+    
+    svg
+      .append("g")
+      .call(d3.axisLeft(yAxis).tickSize(0))
+    
     let myColor = d3
       .scaleSequential()
       .interpolator(d3.interpolateInferno)
       .domain([1, 100]);
-
-    var tooltip = d3
+   
+    
+     var tooltip = d3
       .select("#my_dataviz")
       .append("div")
       .style("opacity", 0)
@@ -56,18 +63,23 @@ d3.json(
       .style("border-width", "2px")
       .style("border-radius", "5px")
       .style("padding", "5px");
-
+    
+   
+   
     // add the squares
     svg
       .selectAll()
       .data(dataset)
+      /*.data(dataset, function (d) {
+        return d.year + ":" + d.month;
+      })*/
       .enter()
       .append("rect")
       .attr("x", function (d) {
-        return xAxis(d.years);
+        return xAxis(d.year);
       })
       .attr("y", function (d) {
-        return yAxis(d.months);
+        return yAxis(d.month);
       })
       .attr("rx", 4)
       .attr("ry", 4)
@@ -80,18 +92,21 @@ d3.json(
       .style("stroke", "none")
       .style("opacity", 0.8)
       .on("mouseover", function (d) {
-        tooltip.style("opacity", 1);
-        d3.select(this).style("stroke", "black").style("opacity", 1);
-      })
+      tooltip.style("opacity", 1);
+      d3.select(this).style("stroke", "black").style("opacity", 1);
+    })
       .on("mousemove", function (d) {
-        tooltip
-          .html("The exact value of<br>this cell is: " + d.value)
-          .style("left", d3.mouse(this)[0] + 70 + "px")
-          .style("top", d3.mouse(this)[1] + "px");
-      })
+      tooltip
+        .html("The exact value of<br>this cell is: " + d.variance)
+        .style("left", d3.mouse(this)[0] + 70 + "px")
+        .style("top", d3.mouse(this)[1] + "px");
+    })
       .on("mouseleave", function (d) {
-        tooltip.style("opacity", 0);
-        d3.select(this).style("stroke", "none").style("opacity", 0.8);
-      });
+      tooltip.style("opacity", 0);
+      d3.select(this).style("stroke", "none").style("opacity", 0.8);
+    })
+
+
+    
   }
-);
+);    
